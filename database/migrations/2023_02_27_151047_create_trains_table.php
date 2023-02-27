22c0555ce561dd8734c7768ea8b067fb24b045dd
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-27 16:32:57 +0100

    update migration and env before i

--- database/migrations/2023_02_27_151047_create_trains_table.php
+++ database/migrations/2023_02_27_151047_create_trains_table.php
@@ -14,6 +14,14 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company', 50);
+            $table->string('dep_station', 50);
+            $table->string('arr_station', 50);
+            $table->time('dep_time');
+            $table->time('arr_time');
+            $table->tinyInteger('code_train')->unsigned();
+            $table->smallInteger('carriage_num')->unsigned();
+            $table->boolean('in_time')->unsigned()->default(1);
+            $table->boolean('is_deleted')->unsigned()->default(0);
             $table->timestamps();
         });
     }
